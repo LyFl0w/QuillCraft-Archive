83906003c9c6b87e86645a8fd0502f49e8982a47
BuildTools <off.lyflow@gmail.com>
2021-10-17 00:32:31 +0200

    patch bug

--- src/main/java/net/quillcraft/build/command/FlightSpeed.java
+++ src/main/java/net/quillcraft/build/command/FlightSpeed.java
@@ -14,8 +14,12 @@ public class FlightSpeed implements CommandExecutor {
         if(commandSender instanceof final Player player && args.length <= 1){
             try{
                 final float speed = args.length == 0 || args[0].isBlank() ? 0.1f : Float.parseFloat(args[0])/10;
+                if(speed > 10){
+                    player.sendMessage("§c"+speed*10+"is too high to fly");
+                    return true;
+                }
                 player.setFlySpeed(speed);
-                player.sendMessage("§2Your flight speed is : §b"+Math.round(speed*10));
+                player.sendMessage("§2Your flight speed is : §b"+speed*10);
                 return true;
             }catch(NumberFormatException exception){
                 player.sendMessage("§cPlease enter a valid number");
