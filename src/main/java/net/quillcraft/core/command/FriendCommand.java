ab9a90fb3b72cfffc374faa800ba9be343528659
lyflow <off.lyflow@gmail.com>
2021-12-14 22:30:01 +0100

    path bugs in FriendCommand.java

--- src/main/java/net/quillcraft/core/command/FriendCommand.java
+++ src/main/java/net/quillcraft/core/command/FriendCommand.java
@@ -2,10 +2,12 @@ package net.quillcraft.core.command;
 
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
+
 import net.quillcraft.core.QuillCraftCore;
 import net.quillcraft.core.manager.LanguageManager;
 import net.quillcraft.core.text.Text;
 import net.quillcraft.core.utils.CommandUtils;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,16 +17,18 @@ import org.bukkit.entity.Player;
 import javax.annotation.Nonnull;
 import java.util.List;
 
-public class FriendCommand implements CommandExecutor, TabCompleter {
+public class FriendCommand implements CommandExecutor, TabCompleter{
 
     private final QuillCraftCore quillCraftCore;
-    private final String[] argsCompletion = new String[]{"add", "remove", "list"};
+    private final String[] argsCompletion = new String[]{"add", "remove", "accept", "deny", "list"};
+
     public FriendCommand(QuillCraftCore quillCraftCore){
         this.quillCraftCore = quillCraftCore;
     }
 
     @Override
     public boolean onCommand(@Nonnull CommandSender cmds, @Nonnull Command cmd, @Nonnull String label, @Nonnull String[] args){
+        if (args.length == 0) return false;
         if(cmds instanceof Player player){
             final String sub = args[0];
             if(args.length == 1){
@@ -37,25 +41,56 @@ public class FriendCommand implements CommandExecutor, TabCompleter {
                 }
                 return false;
             }
+
             if(args.length == 2){
+                final String targetPlayerName = args[1];
                 if(sub.equalsIgnoreCase("add")){
-                    if(args[1].equalsIgnoreCase(player.getName())){
+                    if(targetPlayerName.equalsIgnoreCase(player.getName())){
                         player.sendMessage(LanguageManager.getLanguage(player).getMessage(Text.FRIEND_INVITE_YOUR_SELF));
                         return true;
                     }
                     final ByteArrayDataOutput out = ByteStreams.newDataOutput();
                     out.writeUTF("Add");
+                    out.writeUTF(targetPlayerName);
 
                     player.sendPluginMessage(quillCraftCore, "quillcraft:friend", out.toByteArray());
                     return true;
                 }
-                if(sub.equalsIgnoreCase("remove") || sub.equalsIgnoreCase("rem") ){
-                    if(args[1].equalsIgnoreCase(player.getName())){
+
+                if(sub.equalsIgnoreCase("remove")){
+                    if(targetPlayerName.equalsIgnoreCase(player.getName())){
                         player.sendMessage(LanguageManager.getLanguage(player).getMessage(Text.FRIEND_REMOVE_YOUR_SELF));
                         return true;
                     }
                     final ByteArrayDataOutput out = ByteStreams.newDataOutput();
                     out.writeUTF("Remove");
+                    out.writeUTF(targetPlayerName);
+
+                    player.sendPluginMessage(quillCraftCore, "quillcraft:friend", out.toByteArray());
+                    return true;
+                }
+
+                if(sub.equalsIgnoreCase("accept")){
+                    if(targetPlayerName.equalsIgnoreCase(player.getName())){
+                        player.sendMessage(LanguageManager.getLanguage(player).getMessage(Text.FRIEND_ACCEPT_YOUR_SELF));
+                        return true;
+                    }
+                    final ByteArrayDataOutput out = ByteStreams.newDataOutput();
+                    out.writeUTF("Accept");
+                    out.writeUTF(targetPlayerName);
+
+                    player.sendPluginMessage(quillCraftCore, "quillcraft:friend", out.toByteArray());
+                    return true;
+                }
+
+                if(sub.equalsIgnoreCase("deny")){
+                    if(targetPlayerName.equalsIgnoreCase(player.getName())){
+                        player.sendMessage(LanguageManager.getLanguage(player).getMessage(Text.FRIEND_DENY_YOUR_SELF));
+                        return true;
+                    }
+                    final ByteArrayDataOutput out = ByteStreams.newDataOutput();
+                    out.writeUTF("Deny");
+                    out.writeUTF(targetPlayerName);
 
                     player.sendPluginMessage(quillCraftCore, "quillcraft:friend", out.toByteArray());
                     return true;
