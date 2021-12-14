dcf520caa729ab38e415e3402c2a6979e65dfaba
lyflow <off.lyflow@gmail.com>
2021-12-14 22:43:06 +0100

    commicate the player's status to their friend when he logs in

--- src/main/java/net/quillcraft/bungee/listeners/player/PostLoginListener.java
+++ src/main/java/net/quillcraft/bungee/listeners/player/PostLoginListener.java
@@ -4,6 +4,7 @@ import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.*;
 import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.api.scheduler.TaskScheduler;
 import net.md_5.bungee.event.EventHandler;
 import net.quillcraft.bungee.QuillCraftBungee;
 import net.quillcraft.bungee.manager.LanguageManager;
@@ -11,7 +12,9 @@ import net.quillcraft.bungee.text.Text;
 import net.quillcraft.commons.account.Account;
 import net.quillcraft.commons.account.AccountProvider;
 import net.quillcraft.commons.exception.AccountNotFoundException;
+import net.quillcraft.commons.exception.FriendNotFoundException;
 import net.quillcraft.commons.exception.PartyNotFoundException;
+import net.quillcraft.commons.friend.FriendProvider;
 import net.quillcraft.commons.party.PartyProvider;
 
 public record PostLoginListener(QuillCraftBungee quillCraftBungee) implements Listener {
@@ -20,7 +23,10 @@ public record PostLoginListener(QuillCraftBungee quillCraftBungee) implements Li
     public void onPostLogin(PostLoginEvent event){
         final ProxiedPlayer player = event.getPlayer();
 
-        quillCraftBungee.getProxy().getScheduler().runAsync(quillCraftBungee, () -> {
+        final TaskScheduler taskScheduler = quillCraftBungee.getProxy().getScheduler();
+
+
+        taskScheduler.runAsync(quillCraftBungee, () -> {
             try{
                 final Account account = new AccountProvider(player).getAccount();
                 if(account.hasParty()){
@@ -29,6 +35,14 @@ public record PostLoginListener(QuillCraftBungee quillCraftBungee) implements Li
                                     .replace("%PLAYER%", player.getName()))));
                 }
             }catch(AccountNotFoundException | PartyNotFoundException e){
+                e.printStackTrace();
+            }
+        });
+
+        taskScheduler.runAsync(quillCraftBungee, () -> {
+            try{
+                new FriendProvider(player).getFriends().getOnlineFriends().stream().parallel().forEach(friends -> friends.sendMessage(new TextComponent("Le joueur "+player.getName()+" c'est connecté")));
+            }catch(FriendNotFoundException e){
                 e.printStackTrace();
             }
         });
