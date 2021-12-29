bab341a9c6b02eb12e517ee65df63865b4cdac06
lyflow <off.lyflow@gmail.com>
2021-12-29 04:27:32 +0100

    patch bug

--- src/main/java/net/quillcraft/lobby/listener/player/PlayerInteractListener.java
+++ src/main/java/net/quillcraft/lobby/listener/player/PlayerInteractListener.java
@@ -2,6 +2,7 @@ package net.quillcraft.lobby.listener.player;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import net.quillcraft.commons.account.AccountProvider;
 import net.quillcraft.commons.exception.AccountNotFoundException;
 import net.quillcraft.core.data.management.sql.DatabaseManager;
@@ -27,6 +28,7 @@ import org.bukkit.inventory.ItemStack;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -50,23 +52,25 @@ public class PlayerInteractListener implements Listener {
                         try { // Tentative de connection
                             final Connection connection = DatabaseManager.MINECRAFT_SERVER.getDatabaseAccess().getConnection(); //Ouverture de connection
                             final String uuid = player.getUniqueId().toString();    //Recupere l'uuid du joueur
-                            final PreparedStatement preparedStatementCheck = connection.prepareStatement("SELECT headlist WHERE uuid = ?");// Précontruction d'une requète SQL
+
+                            final PreparedStatement preparedStatementCheck = connection.prepareStatement("SELECT headlist FROM headfinder WHERE uuid = ?");// Précontruction d'une requète SQL
                             preparedStatementCheck.setObject(1,uuid); // Finilisation de la requête
                             preparedStatementCheck.executeQuery(); // Excute et récupere des données
+
                             final ResultSet resultSet = preparedStatementCheck.getResultSet(); // Récupere les données de la commande
-                            if(resultSet.next()){ // Si il y a des données (headlist != null)
-                                final Gson gson = new GsonBuilder().serializeNulls().create();
+
+                            final Gson gson = new GsonBuilder().serializeNulls().create();
+                            if(resultSet.next()){ // Si il y a des données (headlist == null ou headlist = [0, 1, ...])
                                 String headlist = resultSet.getString("headlist");
                                 List<Integer> list = new ArrayList<>();
-                                if(headlist != null){
-                                    list = gson.fromJson(headlist, List.class); // Deserialise string to list
-                                }
-                                if(!list.contains(i)){
-                                    list.add(i);
+                                if(headlist != null) list = gson.fromJson(headlist, new TypeToken<ArrayList<Integer>>(){}.getType()); // Deserialise string to list
+
+                                if(!list.contains(i)){  //Si la tete trouvé n'est pas dans la liste des têtes déjà trouvées
+                                    list.add(i);    // On l'ajoute à la liste
                                     final PreparedStatement preparedStatement =  connection.prepareStatement("UPDATE headfinder SET headlist = ? WHERE uuid = ?"); // Précontruction d'une requète SQL
-                                    preparedStatement.setObject(1, gson.toJson(list)); // Finilisation de la requête / Serialise String to list
+                                    preparedStatement.setObject(1, gson.toJson(list)); // Finilisation de la requête / Serialise List<Integer> to String (-> Json)
                                     preparedStatement.setObject(2, uuid); // Finilisation de la requête
-                                    preparedStatement.execute();    //Execution de la requete
+                                    preparedStatement.executeUpdate();    //Mise à jour de la liste dans la base de donnée
                                 }else{
                                     player.sendMessage("Tête déja trouvé");
                                 }
@@ -74,7 +78,7 @@ public class PlayerInteractListener implements Listener {
                             }else{
                             final PreparedStatement preparedStatement =  connection.prepareStatement("INSERT INTO headfinder (uuid, headlist) VALUES (?, ?)"); // Précontruction d'une requète SQL
                             preparedStatement.setObject(1, uuid); // Finilisation de la requête
-                            preparedStatement.setObject(2, i);  // Finilisation de la requête
+                            preparedStatement.setObject(2, gson.toJson(Collections.singleton(i)));  // Finilisation de la requête
                             preparedStatement.execute();    //Execution de la requete
                             }
                             connection.close(); //Fermeture de la connection
