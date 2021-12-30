66eb3661ae9b623cb40256985d2643935b22d40b
lyflow <off.lyflow@gmail.com>
2021-12-30 19:31:56 +0100

    bug fixing -> filter(String::isBlank)

--- src/main/java/net/quillcraft/bungee/utils/StringUtils.java
+++ src/main/java/net/quillcraft/bungee/utils/StringUtils.java
@@ -8,10 +8,9 @@ public class StringUtils {
     public static boolean containsIgnoreCase(@NonNull List<String> strings, @NonNull String searchStr){
         if(searchStr.isBlank() || strings.size() == 0) return false;
 
-        final String finalSearchStr = searchStr.toLowerCase();
-        return strings.stream().parallel().filter(String::isBlank).anyMatch(string -> string.toLowerCase().contains(finalSearchStr));
+        return strings.stream().parallel().anyMatch(searchStr::equalsIgnoreCase);
     }
-
+    
     public static int indexOfStringIngoreCase(String search, List<String> searchList){
         for(int i = 0; i<searchList.size(); i++){
             if(search.equalsIgnoreCase(searchList.get(i))) return i;
