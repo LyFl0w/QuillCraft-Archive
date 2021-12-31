93fffe8333adc11214ab91339c24718c718681c3
lyflow <off.lyflow@gmail.com>
2021-12-31 01:38:52 +0100

    removed useless "final" to some methodes

--- src/main/java/net/quillcraft/bungee/data/management/redis/RedisCredential.java
+++ src/main/java/net/quillcraft/bungee/data/management/redis/RedisCredential.java
@@ -2,19 +2,19 @@ package net.quillcraft.bungee.data.management.redis;
 
 public record RedisCredential(String ip, String clientName, String password, int database, int port) {
 
-    public final String getPassword(){
+    public String getPassword(){
         return password;
     }
 
-    public final String getClientName(){
+    public String getClientName(){
         return clientName;
     }
 
-    public final int getDatabase(){
+    public int getDatabase(){
         return database;
     }
 
-    public final String getAdress(){
+    public String getAdress(){
         return "redis://"+ip+":"+port;
     }
 
