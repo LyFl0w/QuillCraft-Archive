f03052fa02620be9d965bc1a0ea9420c42d34900
lyflow <off.lyflow@gmail.com>
2021-12-31 02:07:05 +0100

    optimisation and adjustment of the flexibility of the serialization / deserialization code

--- src/main/java/net/quillcraft/bungee/serialization/ProfileSerializationAccount.java
+++ src/main/java/net/quillcraft/bungee/serialization/ProfileSerializationAccount.java
@@ -1,17 +1,21 @@
 package net.quillcraft.bungee.serialization;
 
+import com.google.gson.reflect.TypeToken;
 import net.quillcraft.commons.account.Account;
 
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ProfileSerializationAccount {
 
+    @SuppressWarnings("unchecked")
     public static class Particle extends ProfileSerialization {
         @Override
-        public HashMap<Account.Particles, Boolean> deserialize(String json) {
-            final HashMap<String, Boolean> particlesString = gson.fromJson(json, HashMap.class);
+        public HashMap<Account.Particles, Boolean> deserialize(@Nonnull String json) {
             final HashMap<Account.Particles, Boolean> particles = new HashMap<>();
-            particlesString.forEach((key, value) -> particles.put(Account.Particles.valueOf(key), value));
+            ((HashMap<String, Boolean>)gson.fromJson(json, new TypeToken<HashMap<String, Boolean>>(){}.getType()))
+                    .forEach((key, value) -> particles.put(Account.Particles.valueOf(key), value));
 
             return particles;
         }
--- src/main/java/net/quillcraft/bungee/serialization/ProfileSerializationUtils.java
+++ src/main/java/net/quillcraft/bungee/serialization/ProfileSerializationUtils.java
@@ -1,5 +1,8 @@
 package net.quillcraft.bungee.serialization;
 
+import com.google.gson.reflect.TypeToken;
+
+import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -8,17 +11,18 @@ public class ProfileSerializationUtils {
 
     public static class ListString extends ProfileSerialization{
         @Override
-        public List<String> deserialize(String json){
-            return gson.fromJson(json, List.class);
+        public List<String> deserialize(@Nonnull String json){
+            return gson.fromJson(json, new TypeToken<ArrayList<String>>(){}.getType());
         }
     }
 
+    @SuppressWarnings("unchecked")
     public static class ListUUID extends ProfileSerialization{
         @Override
-        public List<UUID> deserialize(String json){
+        public List<UUID> deserialize(@Nonnull String json){
             final List<UUID> uuidList = new ArrayList<>();
-            gson.fromJson(json, List.class).
-                    forEach(uuidString -> uuidList.add(UUID.fromString((String) uuidString)));
+            ((List<String>) gson.fromJson(json, new TypeToken<ArrayList<String>>(){}.getType()))
+                    .forEach(uuidString -> uuidList.add(UUID.fromString(uuidString)));
             return uuidList;
         }
     }
