938b5f4dee22d3e3f205ad025884bacb3c97355d
lyflow <off.lyflow@gmail.com>
2022-01-11 00:00:07 +0100

    optimize data storage of logger with static variable

--- src/main/java/org/lumy/Lumy.java
+++ src/main/java/org/lumy/Lumy.java
@@ -1,19 +1,21 @@
 package org.lumy;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 import org.lumy.api.utils.FileUtils;
 import org.lumy.server.LumyServer;
-import org.lumy.server.data.RedisManager;
-import org.apache.logging.log4j.LogManager;
 
 public class Lumy{
 
+    public final static Logger logger = LogManager.getLogger("Lumy");
+
     public static void main(String[] args){
         try{
-            RedisManager.TEXT.getRedisAccess().init();
             new LumyServer(FileUtils.getObjectFromYamlFile(
                     FileUtils.getFileFromResource( "config-lumy.yml"), Lumy.Configuration.class));
         }catch(Exception e){
-            LogManager.getLogger("lumy").error(e.getMessage(), e);
+            logger.error(e.getMessage(), e);
         }
     }
     
