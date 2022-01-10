5560a8f3093d0c5958a5b6bd20d6559b4de71c3d
lyflow <off.lyflow@gmail.com>
2022-01-10 17:46:46 +0100

    patch leak connexion

--- src/main/java/net/quillcraft/lobby/headfinder/HeadFinderProvider.java
+++ src/main/java/net/quillcraft/lobby/headfinder/HeadFinderProvider.java
@@ -3,8 +3,10 @@ package net.quillcraft.lobby.headfinder;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+
 import net.quillcraft.core.data.management.redis.RedisManager;
 import net.quillcraft.core.data.management.sql.DatabaseManager;
+
 import org.bukkit.entity.Player;
 import org.redisson.api.RBucket;
 import org.redisson.api.RedissonClient;
@@ -14,71 +16,70 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.UUID;
 
-public class HeadFinderProvider {
+public class HeadFinderProvider{
+
     private final String uuid, keyHeadList;
-    private List<Integer> headlist;
-    private final RedissonClient redissonClient;
+    private final List<Integer> headlist;
+    private final static RedissonClient redissonClient = RedisManager.ACCOUNT.getRedisAccess().getRedissonClient();
 
-
-    public HeadFinderProvider(Player player) {
-         uuid = player.getUniqueId().toString();
-         headlist = getHeadList();
-         keyHeadList = "headlist:"+uuid;
-        this.redissonClient = RedisManager.PLAYER_DATA.getRedisAccess().getRedissonClient();
+    public HeadFinderProvider(Player player){
+        uuid = player.getUniqueId().toString();
+        keyHeadList = "headlist:"+uuid;
+        this.headlist = getHeadList();
     }
 
     private List<Integer> getHeadList(){
-       headlist = getHeadListFromRedis();
-       if(headlist == null){
-           headlist = getHeadListFromDatabase();
-           updateHeadListInRedis(headlist);
-       }
-       return headlist;
+        List<Integer> tempHeadlist = getHeadListFromRedis();
+        if(tempHeadlist == null){
+            tempHeadlist = getHeadListFromDatabase();
+            updateHeadListInRedis(tempHeadlist);
+        }
+        return tempHeadlist;
     }
 
     private List<Integer> getHeadListFromDatabase(){
         try{
             final Connection connection = DatabaseManager.MINECRAFT_SERVER.getDatabaseAccess().getConnection(); //Ouverture de connection
             final PreparedStatement preparedStatementCheck = connection.prepareStatement("SELECT headlist FROM headfinder WHERE uuid = ?");// Précontruction d'une requète SQL
-            preparedStatementCheck.setObject(1,uuid); // Finilisation de la requête
+            preparedStatementCheck.setObject(1, uuid); // Finilisation de la requête
             preparedStatementCheck.executeQuery(); // Execute et récupere des données
             final ResultSet resultSet = preparedStatementCheck.getResultSet(); // Récupere les données de la commande
-            connection.close();
             final Gson gson = new GsonBuilder().serializeNulls().create();
             if(resultSet.next()){
                 String headlist = resultSet.getString("headlist");
-                if(headlist != null) {
+                connection.close();
+                if(headlist != null){
                     return gson.fromJson(headlist, new TypeToken<ArrayList<Integer>>(){}.getType());
                 }else{
                     return new ArrayList<>();
                 }
             }else{
+                connection.close();
                 createHeadListInDatabase();
                 return new ArrayList<>();
             }
 
-        } catch (SQLException e) {
+        }catch(SQLException e){
             e.printStackTrace();
         }
         return null;
     }
 
-    public List<Integer> getHeadListFromRedis(){
-        return null;
+    private List<Integer> getHeadListFromRedis(){
+        final RBucket<List<Integer>> headListRBucket = redissonClient.getBucket(keyHeadList);
+        return headListRBucket.get();
     }
 
     private void createHeadListInDatabase(){
         try{
             final Connection connection = DatabaseManager.MINECRAFT_SERVER.getDatabaseAccess().getConnection(); //Ouverture de connection
-            final PreparedStatement preparedStatement =  connection.prepareStatement("INSERT INTO headfinder (uuid) VALUES (?)"); // Précontruction d'une requète SQ
+            final PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO headfinder (uuid) VALUES (?)"); // Précontruction d'une requète SQ
             preparedStatement.setObject(1, uuid); // Finilisation de la requête
             preparedStatement.execute();    //Execution de la requete
             connection.close();
-        }catch (SQLException e) {
+        }catch(SQLException e){
             e.printStackTrace();
         }
     }
@@ -87,19 +88,14 @@ public class HeadFinderProvider {
         try{
             final Gson gson = new GsonBuilder().serializeNulls().create();
             final Connection connection = DatabaseManager.MINECRAFT_SERVER.getDatabaseAccess().getConnection();
-            final PreparedStatement preparedStatement =  connection.prepareStatement("UPDATE headfinder SET headlist = ? WHERE uuid = ?"); // Précontruction d'une requète SQL
+            final PreparedStatement preparedStatement = connection.prepareStatement("UPDATE headfinder SET headlist = ? WHERE uuid = ?"); // Précontruction d'une requète SQL
             preparedStatement.setObject(1, gson.toJson(headlist)); // Finilisation de la requête / Serialise List<Integer> to String (-> Json)
             preparedStatement.setObject(2, uuid); // Finilisation de la requête
             preparedStatement.executeUpdate();    //Mise à jour de la liste dans la bdd
             connection.close();
-        }catch (SQLException e) {
+        }catch(SQLException e){
             e.printStackTrace();
         }
-    }
-    public void updateHeadList(){
-        updateHeadListInRedis(headlist);
-        updateHeadListDatabase();
-
     }
 
     private void updateHeadListInRedis(List<Integer> headlist){
@@ -107,11 +103,13 @@ public class HeadFinderProvider {
         headListRBucket.set(headlist);
     }
 
-    public List<Integer> getHeadlist() {
+    public void updateHeadList(){
+        updateHeadListInRedis(headlist);
+        updateHeadListDatabase();
+    }
+
+    public List<Integer> getHeadlist(){
         return headlist;
     }
-    public List<Integer> getHeadListInRedis(){
-        final RBucket<List<Integer>> headListRBucket = redissonClient.getBucket(keyHeadList);
-        return headListRBucket.get();
-    }
+
 }
