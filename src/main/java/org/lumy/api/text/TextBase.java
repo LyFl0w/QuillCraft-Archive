80cebdf148bb681d1e8a46f8d6aeb612c79ac728
lyflow <off.lyflow@gmail.com>
2022-01-12 18:00:38 +0100

    patch bug ( file not found for config-lumy.yml)

--- src/main/java/org/lumy/api/text/TextBase.java
+++ src/main/java/org/lumy/api/text/TextBase.java
@@ -4,7 +4,6 @@ public sealed interface TextBase permits Text, TextList{
 
     String getPath();
 
-
     default boolean isDefaultText(){
         return ((Enum<?>)this).name().startsWith("SERVER");
     }
