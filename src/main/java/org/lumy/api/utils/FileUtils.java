3db48fa646403ca8c112dd250a2c1b2436366482
lyflow <off.lyflow@gmail.com>
2022-01-15 19:31:42 +0100

    add function to getFileFromResource

--- src/main/java/org/lumy/api/utils/FileUtils.java
+++ src/main/java/org/lumy/api/utils/FileUtils.java
@@ -11,20 +11,34 @@ import java.util.Objects;
 public class FileUtils{
 
     public static File getFileFromResource(File parent, String fileName){
+        return getFileFromResource(new File(parent, fileName));
+    }
+
+    public static File getFileFromResource(String fileName){
         try{
-            final File file = new File(parent, fileName);
-            if(!file.getParentFile().exists()) file.getParentFile().mkdirs();
+            final File file = new File(fileName);
+            if(file.getParentFile() != null && !file.getParentFile().exists()) file.getParentFile().mkdirs();
             if(!file.exists()) Files.copy(Objects.requireNonNull(FileUtils.class.getClassLoader().getResourceAsStream(fileName)), file.toPath());
 
-            System.out.println("return file");
             return file;
         }catch(Exception e){
             e.printStackTrace();
-            System.out.println("null exceptio");
         }
-        System.out.println("noul");
         return null;
     }
+
+    private static File getFileFromResource(File file){
+        try{
+            if(!file.getParentFile().exists()) file.getParentFile().mkdirs();
+            if(!file.exists()) Files.copy(Objects.requireNonNull(FileUtils.class.getClassLoader().getResourceAsStream(file.getName())), file.toPath());
+
+            return file;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 
     public static <T> T getObjectFromYamlFile(File file, Class<T> valueType) throws IOException{
         return new ObjectMapper(new YAMLFactory()).readValue(file, valueType);
