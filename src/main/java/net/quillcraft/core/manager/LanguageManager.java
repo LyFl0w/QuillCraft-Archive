100901ff8864a64a4e26f101657e69502e13beff
lyflow <off.lyflow@gmail.com>
2022-01-17 12:42:23 +0100

    add LanguageManager.DEFAULT system

--- src/main/java/net/quillcraft/core/manager/LanguageManager.java
+++ src/main/java/net/quillcraft/core/manager/LanguageManager.java
@@ -39,7 +39,7 @@ public enum LanguageManager {
         }catch(AccountNotFoundException e){
             e.printStackTrace();
         }
-        return LanguageManager.ENGLISH_US;
+        return LanguageManager.DEFAULT;
     }
 
     public static LanguageManager getLanguage(final Account account){
