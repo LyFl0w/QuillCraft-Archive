5490a876d356668a1bc4ad2eb8d28e25ae90bfce
lyflow <off.lyflow@gmail.com>
2022-01-26 22:52:21 +0100

    rename FlightSpeed.java to FlightSpeedCommand.java

--- src/main/java/net/quillcraft/build/command/FlightSpeedCommand.java
+++ src/main/java/net/quillcraft/build/command/FlightSpeedCommand.java
@@ -7,7 +7,7 @@ import org.bukkit.entity.Player;
 
 import javax.annotation.Nonnull;
 
-public class FlightSpeed implements CommandExecutor {
+public class FlightSpeedCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@Nonnull CommandSender commandSender, @Nonnull Command command, @Nonnull String label, @Nonnull String[] args){
