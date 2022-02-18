3d4d7ca18ce9ee5f4888d948348dd9cd4f224845
lyflow <off.lyflow@gmail.com>
2022-02-18 14:39:49 +0100

    add Essential data

--- src/main/java/net/quillcraft/commons/game/GameEnum.java
+++ src/main/java/net/quillcraft/commons/game/GameEnum.java
@@ -2,6 +2,15 @@ package net.quillcraft.commons.game;
 
 public enum GameEnum{
 
-    PARKOUR_PVP_SOLO;
+    PARKOUR_PVP_SOLO("Parkour PvP (solo)");
 
+    private final String gameName;
+
+    GameEnum(String gameName){
+        this.gameName = gameName;
+    }
+
+    public String getGameName(){
+        return gameName;
+    }
 }
