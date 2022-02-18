ef723c0b6750e55739637e581d66c4feb3614ba7
lyflow <off.lyflow@gmail.com>
2022-02-18 14:36:46 +0100

    add essential data in Subscriber
    add removeAllSubscribersData function ro remove every WaitingList

--- src/main/java/net/quillcraft/bungee/subscriber/SubscriberManager.java
+++ src/main/java/net/quillcraft/bungee/subscriber/SubscriberManager.java
@@ -1,14 +1,26 @@
 package net.quillcraft.bungee.subscriber;
 
+import net.quillcraft.bungee.QuillCraftBungee;
+import net.quillcraft.commons.game.GameEnum;
+import net.quillcraft.commons.game.WaitingList;
+
 import java.util.Arrays;
 
 public enum SubscriberManager{
 
-    SUBSCRIBER_GAME(new SubscriberGame());
+    SUBSCRIBER_GAME(new SubscriberGame(QuillCraftBungee.getInstance().getProxy()));
 
     private final Subscriber subscriber;
     SubscriberManager(Subscriber subscriber){
         this.subscriber = subscriber;
+    }
+
+    public static void removeAllSubscribersData(){
+        Arrays.stream(GameEnum.values()).parallel().forEach(gameEnum -> {
+            final WaitingList waitingList = new WaitingList(gameEnum);
+            waitingList.getWaitersList().clear();
+            waitingList.updateWaitersListRedis();
+        });
     }
 
     public Subscriber getSubscriber(){
