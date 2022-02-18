88ba362e65077c36566fd3781a55bc2b40a3d9af
lyflow <off.lyflow@gmail.com>
2022-02-18 14:38:06 +0100

    patch bug
    send player to the good server

--- src/main/java/net/quillcraft/bungee/subscriber/Subscriber.java
+++ src/main/java/net/quillcraft/bungee/subscriber/Subscriber.java
@@ -1,9 +1,15 @@
 package net.quillcraft.bungee.subscriber;
 
+import net.md_5.bungee.api.ProxyServer;
 import net.quillcraft.bungee.data.management.redis.RedisManager;
 import org.redisson.api.RedissonClient;
 
 public abstract class Subscriber{
+
+    protected final ProxyServer proxyServer;
+    public Subscriber(ProxyServer proxyServer){
+        this.proxyServer = proxyServer;
+    }
 
     protected final static RedissonClient redissonClient = RedisManager.GAME_SERVER.getRedisAccess().getRedissonClient();
 
