59624b0dde96d13e3cf5503849fc10d7cecfefa3
lyflow <off.lyflow@gmail.com>
2022-02-21 16:51:52 +0100

    add ScoreboardBuilderHashMap to stock scoreboardBuilder

--- src/main/java/net/quillcraft/parkourpvp/ParkourPvP.java
+++ src/main/java/net/quillcraft/parkourpvp/ParkourPvP.java
@@ -4,14 +4,18 @@ import net.quillcraft.commons.game.GameProperties;
 import net.quillcraft.commons.game.GeneralGameStatus;
 import net.quillcraft.commons.game.ParkourPvPGame;
 import net.quillcraft.core.utils.builders.YamlConfigurationBuilder;
+import net.quillcraft.core.utils.builders.scoreboard.ScoreboardBuilder;
 import net.quillcraft.parkourpvp.manager.PluginManager;
 
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
 
 public class ParkourPvP extends JavaPlugin{
 
     private static ParkourPvP INSTANCE;
 
+    private final HashMap<String, ScoreboardBuilder> scoreboardBuilderHashMap = new HashMap<>();
     private ParkourPvPGame parkourPvPGame;
 
     @Override
@@ -26,6 +30,7 @@ public class ParkourPvP extends JavaPlugin{
 
         parkourPvPGame.setGameStatus(GeneralGameStatus.PLAYER_WAITING);
         parkourPvPGame.updateRedis();
+
         parkourPvPGame.searchPlayer();
 
         getLogger().info("Plugin ParkourPvP enable");
@@ -35,6 +40,10 @@ public class ParkourPvP extends JavaPlugin{
     public void onDisable(){
         parkourPvPGame.deleteRedisKey();
         getLogger().info("Plugin ParkourPvP disable");
+    }
+
+    public HashMap<String, ScoreboardBuilder> getScoreboardBuilderHashMap(){
+        return scoreboardBuilderHashMap;
     }
 
     public ParkourPvPGame getParkourPvPGame(){
