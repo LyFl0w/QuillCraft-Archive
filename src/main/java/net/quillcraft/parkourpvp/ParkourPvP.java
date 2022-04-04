dbd00c7dfabd60d43876b3ef7e69c4cadee89332
lyflow <off.lyflow@gmail.com>
2022-04-04 17:19:30 +0200

    fix -> first step : add information about ParkourPvPGame        second step : generate information for the game (spawn , make copy of map, etc)

--- src/main/java/net/quillcraft/parkourpvp/ParkourPvP.java
+++ src/main/java/net/quillcraft/parkourpvp/ParkourPvP.java
@@ -4,18 +4,13 @@ import net.quillcraft.commons.game.GameProperties;
 import net.quillcraft.commons.game.GeneralGameStatus;
 import net.quillcraft.commons.game.ParkourPvPGame;
 import net.quillcraft.core.utils.builders.YamlConfigurationBuilder;
-import net.quillcraft.core.utils.builders.scoreboard.ScoreboardBuilder;
 import net.quillcraft.parkourpvp.manager.PluginManager;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.HashMap;
-
 public class ParkourPvP extends JavaPlugin{
 
     private static ParkourPvP INSTANCE;
-
-    private final HashMap<String, ScoreboardBuilder> scoreboardBuilderHashMap = new HashMap<>();
 
     private GameData gameData;
 
@@ -25,11 +20,11 @@ public class ParkourPvP extends JavaPlugin{
     public void onEnable(){
         INSTANCE = this;
 
-        gameData = new GameData(this);
-
         parkourPvPGame = new ParkourPvPGame(
                 new YamlConfigurationBuilder(this, "default_game_setting.yml", true).getConfig().getInt("id"),
                 new GameProperties(new YamlConfigurationBuilder(this, "game_properties.yml", true).getConfig()));
+
+        gameData = new GameData(this);
 
         new PluginManager(this);
 
@@ -47,15 +42,15 @@ public class ParkourPvP extends JavaPlugin{
         getLogger().info("Plugin ParkourPvP disable");
     }
 
-    public HashMap<String, ScoreboardBuilder> getScoreboardBuilderHashMap(){
-        return scoreboardBuilderHashMap;
-    }
-
     public ParkourPvPGame getParkourPvPGame(){
         return parkourPvPGame;
     }
 
     public static ParkourPvP getInstance(){
         return INSTANCE;
+    }
+
+    public GameData getGameData(){
+        return gameData;
     }
 }
