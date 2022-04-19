6634beab684fdf891fbbdb331055073ff69e54fa
lyflow <off.lyflow@gmail.com>
2022-04-19 20:33:21 +0200

    add id to CheckPoint add function(-> addPlayer) to add a player to the list that returns a message to be displayed to the player when he catches a point.
    remove bonus variable to set it in CheckPointCoinsBonus.java with some utils variable

--- src/main/java/net/quillcraft/parkourpvp/game/CheckPoint.java
+++ src/main/java/net/quillcraft/parkourpvp/game/CheckPoint.java
@@ -7,14 +7,16 @@ import java.util.UUID;
 
 public class CheckPoint{
 
-    public static int coins, bonusFirst, bonusSecond, bonusThird;
+    public static int coins;
 
+    private final int id;
     private final ArrayList<UUID> players;
     private final Location location;
 
-    public CheckPoint(Location location){
+    public CheckPoint(Location location, int id){
         this.location = location;
         this.players = new ArrayList<>();
+        this.id = id;
     }
 
     public Location getLocation(){
@@ -24,4 +26,29 @@ public class CheckPoint{
     public ArrayList<UUID> getPlayers(){
         return players;
     }
+
+    public int getId(){
+        return id;
+    }
+
+    public String addPlayer(PlayerData playerData){
+        final StringBuilder message = new StringBuilder();
+
+        players.add(playerData.getUuid());
+        playerData.addCoins(coins);
+
+        message.append(coins).append(" coins");
+
+        final int size = players.size();
+        if(size <= 3){
+            final int bonus = CheckPointCoinsBonus.getBonus(size);
+            if(bonus != 0){
+                playerData.addCoins(bonus);
+                message.append(" + ").append(bonus).append(" coins for bonus position");
+            }
+        }
+
+        return message.toString();
+    }
+
 }
---
+++ src/main/java/net/quillcraft/parkourpvp/game/CheckPointCoinsBonus.java
@@ -0,0 +1,32 @@
+package net.quillcraft.parkourpvp.game;
+
+import java.util.Arrays;
+
+public enum CheckPointCoinsBonus{
+
+    BONUS_FIRST(1, 0), BONUS_SECOND(2, 0),  BONUS_THIRD(3, 0);
+
+    private final int position;
+    private int additionalCoins;
+
+    CheckPointCoinsBonus(int position, int additionalCoins){
+        this.position = position;
+        this.additionalCoins = additionalCoins;
+    }
+
+    public int getPosition(){
+        return position;
+    }
+
+    public int getAdditionalCoins(){
+        return additionalCoins;
+    }
+
+    public void setAdditionalCoins(int additionalCoins){
+        this.additionalCoins = additionalCoins;
+    }
+
+    public static int getBonus(int place){
+        return Arrays.stream(values()).filter(checkPointCoinsBonus -> checkPointCoinsBonus.getPosition() == place).findFirst().get().getAdditionalCoins();
+    }
+}
