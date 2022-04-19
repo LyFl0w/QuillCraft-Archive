3b1efdf9f1a5a3282d75d6163140fa2584fdc700
lyflow <off.lyflow@gmail.com>
2022-04-19 20:42:36 +0200

    add function InGameStatus.actualInGameStatusIs to easily check if the game status is one of these variables

--- src/main/java/net/quillcraft/parkourpvp/status/InGameStatus.java
+++ src/main/java/net/quillcraft/parkourpvp/status/InGameStatus.java
@@ -2,6 +2,10 @@ package net.quillcraft.parkourpvp.status;
 
 public enum InGameStatus{
 
-    WAIT, WAITING_BEFORE_JUMP, JUMP, WAITING_AFTER_JUMP, WAITING_BEFORE_PVP, PVP, WAITING_AFTER_PVP, END
+    WAIT, WAITING_BEFORE_JUMP, JUMP, WAITING_AFTER_JUMP, WAITING_BEFORE_PVP, PVP, WAITING_AFTER_PVP, END;
+
+    public boolean actualInGameStatusIs(InGameStatus inGameStatus) {
+        return this == inGameStatus;
+    }
 
 }
