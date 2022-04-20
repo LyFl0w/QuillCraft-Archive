68d29e2173ba2048d6db1b6adfe77d659734baec
lyflow <off.lyflow@gmail.com>
2022-04-20 13:17:39 +0200

    add -> teleport player to checkpoint if he take damage

---
+++ src/main/java/net/quillcraft/parkourpvp/listener/entity/EntityDamageListener.java
@@ -0,0 +1,31 @@
+package net.quillcraft.parkourpvp.listener.entity;
+
+import net.quillcraft.parkourpvp.manager.GameManager;
+import net.quillcraft.parkourpvp.ParkourPvP;
+
+import net.quillcraft.parkourpvp.status.InGameStatus;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+public class EntityDamageListener implements Listener{
+
+    private final ParkourPvP parkourPvP;
+    public EntityDamageListener(ParkourPvP parkourPvP){
+        this.parkourPvP = parkourPvP;
+    }
+
+    @EventHandler
+    public void onEntityDamageEvent(EntityDamageEvent event){
+
+        if(event.getEntity() instanceof final Player player){
+            final GameManager gameManager = parkourPvP.getGameData();
+
+            if(gameManager.getInGameStatus().actualInGameStatusIs(InGameStatus.PVP)) return;
+
+            player.teleport(gameManager.getCheckPoints().get(gameManager.getPlayersData().get(player.getName()).getCheckPointID()).getLocation());
+            event.setCancelled(true);
+        }
+    }
+}
