5cc759bd364a231dcc0f9e88cf88dc52f69826f3
finkitpiat <romain.meyer.fkpt@gmail.com>
2022-04-20 18:33:30 +0200

    Add texture for heads

--- src/main/java/net/quillcraft/lobby/QuillCraftLobby.java
+++ src/main/java/net/quillcraft/lobby/QuillCraftLobby.java
@@ -1,21 +1,27 @@
 package net.quillcraft.lobby;
 
+import me.arcaniax.hdb.api.DatabaseLoadEvent;
+import me.arcaniax.hdb.api.HeadDatabaseAPI;
 import net.lyflow.songapi.manager.SongManager;
 import net.quillcraft.lobby.manager.PluginManager;
 import net.quillcraft.lobby.npc.NPCManager;
 import org.bukkit.command.PluginCommand;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.annotation.Nonnull;
 import java.util.Objects;
 
-public class QuillCraftLobby extends JavaPlugin {
+public class QuillCraftLobby extends JavaPlugin implements Listener {
 
     private static QuillCraftLobby INSTANCE;
     //private ProtocolManager protocolManager;
 
     private SongManager songManager;
     private NPCManager npcManager;
+    private HeadDatabaseAPI headDatabaseAPI;
 
     @Override
     public void onEnable(){
@@ -26,6 +32,7 @@ public class QuillCraftLobby extends JavaPlugin {
         //this.protocolManager = ProtocolLibrary.getProtocolManager();
         this.npcManager = new NPCManager(this,120);
         this.songManager = new SongManager(this);
+        this.getServer().getPluginManager().registerEvents(this, this);
 
         new PluginManager(this);
     }
@@ -51,4 +58,14 @@ public class QuillCraftLobby extends JavaPlugin {
     public PluginCommand getCommand(@Nonnull String name){
         return Objects.requireNonNull(super.getCommand(name));
     }
+
+    public HeadDatabaseAPI getHeadDatabaseAPI() {
+        return headDatabaseAPI;
+    }
+
+    @EventHandler
+    public void onDatabaseLoad(DatabaseLoadEvent e) {
+        headDatabaseAPI = new HeadDatabaseAPI();
+    }
+
 }
--- src/main/java/net/quillcraft/lobby/command/HeadCommand.java
+++ src/main/java/net/quillcraft/lobby/command/HeadCommand.java
@@ -1,7 +1,10 @@
 package net.quillcraft.lobby.command;
 
+import me.arcaniax.hdb.api.HeadDatabaseAPI;
+
+import net.quillcraft.lobby.QuillCraftLobby;
 import net.quillcraft.lobby.manager.ConfigurationManager;
-import org.bukkit.Location;
+
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
@@ -10,11 +13,9 @@ import org.bukkit.block.data.Rotatable;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.Configuration;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.meta.SkullMeta;
 
 import javax.annotation.Nonnull;
 
@@ -23,36 +24,49 @@ public class HeadCommand implements CommandExecutor {
     final BlockFace[] blockFaces = {BlockFace.NORTH, BlockFace.NORTH_NORTH_EAST, BlockFace.NORTH_EAST, BlockFace.EAST_NORTH_EAST,
             BlockFace.EAST, BlockFace.EAST_SOUTH_EAST, BlockFace.SOUTH_EAST, BlockFace.SOUTH_SOUTH_EAST, BlockFace.SOUTH,
             BlockFace.SOUTH_SOUTH_WEST, BlockFace.WEST_SOUTH_WEST, BlockFace.SOUTH_WEST, BlockFace.WEST, BlockFace.WEST_NORTH_WEST, BlockFace.NORTH_WEST, BlockFace.NORTH_NORTH_WEST};
+    private final QuillCraftLobby quillCraftLobby;
+
+    public HeadCommand(QuillCraftLobby main) {
+        this.quillCraftLobby = main;
+    }
 
     @Override
     public boolean onCommand(@Nonnull CommandSender commandSender, @Nonnull Command command, @Nonnull String label, @Nonnull String[] args) {
         if(commandSender instanceof final Player player){
-            if(args.length == 3){
-                try{
-                    final int headYaw = Integer.parseInt(args[1]);
-                    final int coins = Integer.parseInt(args[2]);
-                    if(headYaw > 15) return sendError(player);
+            if(args.length == 3) {
+                try {
+                    final int headYaw = Integer.parseInt(args[0]);
+                    final int coins = Integer.parseInt(args[1]);
+                    final String headID = args[2];
+                    final HeadDatabaseAPI headDatabaseAPI = quillCraftLobby.getHeadDatabaseAPI();
+                    if (!headDatabaseAPI.isHead(headID)){
+                        player.sendMessage("L'ID de la tête n'est pas reconnu");
+                        return true;
+                    }
+
+
+                    if (headYaw > 15) return sendError(player);
 
                     final Block block = player.getWorld().getBlockAt(player.getLocation());
-                    if(block.getType() == Material.AIR){
+                    if (block.getType() == Material.AIR) {
                         final String worldName = player.getWorld().getName();
                         final StringBuilder pathBuilder = new StringBuilder(worldName);
                         final FileConfiguration headConfiguration = ConfigurationManager.HEAD.getConfiguration();
                         final ConfigurationSection configurationSection = headConfiguration.getConfigurationSection(worldName);
 
-
                         //Save head in config file
                         pathBuilder.append(".").append(configurationSection == null ? 0 : configurationSection.getKeys(false).size()).append(".");
 
-                        headConfiguration.set(pathBuilder+"x", player.getLocation().getBlockX());
-                        headConfiguration.set(pathBuilder+"y", player.getLocation().getBlockY());
-                        headConfiguration.set(pathBuilder+"z", player.getLocation().getBlockZ());
-                        headConfiguration.set(pathBuilder+"yaw", headYaw);
-                        headConfiguration.set(pathBuilder+"coins", coins);
+                        headConfiguration.set(pathBuilder + "x", player.getLocation().getBlockX());
+                        headConfiguration.set(pathBuilder + "y", player.getLocation().getBlockY());
+                        headConfiguration.set(pathBuilder + "z", player.getLocation().getBlockZ());
+                        headConfiguration.set(pathBuilder + "yaw", headYaw);
+                        headConfiguration.set(pathBuilder + "coins", coins);
                         ConfigurationManager.HEAD.saveFile();
 
                         //Place head
                         block.setType(Material.PLAYER_HEAD);
+                        headDatabaseAPI.setBlockSkin(block, headID);
 
                         //Rotation
                         final Skull thisSkull = (Skull) player.getWorld().getBlockAt(player.getLocation()).getState();
@@ -66,7 +80,8 @@ public class HeadCommand implements CommandExecutor {
                     }
                     player.sendMessage("Il y a un block la où vous vous trouver");
                     return true;
-                }catch(Exception exception){
+
+                } catch (Exception exception) {
                     return sendError(player);
                 }
             }
@@ -78,4 +93,5 @@ public class HeadCommand implements CommandExecutor {
         player.sendMessage("Sélectionner une rotation de tête valide (0-15)");
         return true;
     }
+
 }
