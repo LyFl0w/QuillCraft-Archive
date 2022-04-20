e8c678dcc245bf27105398490e23238d996327cf
lyflow <off.lyflow@gmail.com>
2022-04-20 13:19:58 +0200

    add -> interaction with slime ball

--- src/main/java/net/quillcraft/parkourpvp/listener/player/PlayerInteractListener.java
+++ src/main/java/net/quillcraft/parkourpvp/listener/player/PlayerInteractListener.java
@@ -1,11 +1,15 @@
 package net.quillcraft.parkourpvp.listener.player;
 
+import net.quillcraft.parkourpvp.manager.GameManager;
 import net.quillcraft.parkourpvp.ParkourPvP;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
 
 public class PlayerInteractListener implements Listener{
 
@@ -18,11 +22,19 @@ public class PlayerInteractListener implements Listener{
     public void onPLayerInteractEvent(PlayerInteractEvent event){
         if(event.getHand() == EquipmentSlot.OFF_HAND) return;
 
-        switch(parkourPvP.getGameData().getInGameStatus()){
-            //TODO : Interaction with respawn item
-            case JUMP -> {}
-            //TODO : Interaction with item to get shop inventory
-            case WAITING_BEFORE_PVP -> {}
+        final ItemStack itemStack = event.getItem();
+        if(itemStack == null || itemStack.getType() == Material.AIR) return;
+
+        final Player player = event.getPlayer();
+        final GameManager gameManager = parkourPvP.getGameData();
+
+        switch(gameManager.getInGameStatus()){
+            // Interaction with respawn item
+            case JUMP -> {
+                if(itemStack.getType() == Material.SLIME_BALL){
+                    player.teleport(gameManager.getCheckPoints().get(gameManager.getPlayersData().get(player.getName()).getCheckPointID()).getLocation());
+                }
+            }
         }
 
     }
