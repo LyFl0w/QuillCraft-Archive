0dcc6fbb436119942097f5c38cd5967199f965c9
lyflow <off.lyflow@gmail.com>
2022-04-21 12:22:10 +0200

    add -> BlockBreakListener.java to removes the possibility for the player to break blocks other than those placed by players.
    add -> BlockPlaceListener.java to places blocks not higher than the maximum set in yaml file.

---
+++ src/main/java/net/quillcraft/parkourpvp/listener/block/BlockBreakListener.java
@@ -0,0 +1,33 @@
+package net.quillcraft.parkourpvp.listener.block;
+
+import net.quillcraft.parkourpvp.ParkourPvP;
+import net.quillcraft.parkourpvp.status.InGameStatus;
+import org.bukkit.Location;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+import java.util.ArrayList;
+
+public class BlockBreakListener implements Listener{
+
+    private final ArrayList<Location> blocksPlaced;
+    private final ParkourPvP parkourPvP;
+    public BlockBreakListener(ParkourPvP parkourPvP, BlockPlaceListener blockPlaceListener){
+        this.parkourPvP = parkourPvP;
+        this.blocksPlaced = blockPlaceListener.getBlocksPlaced();
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event){
+        if(!parkourPvP.getGameManager().getInGameStatus().actualInGameStatusIs(InGameStatus.PVP)) event.setCancelled(true);
+
+        // BREAK ONLY BLOC PLACED BY PLAYER
+        final Location location = event.getBlock().getLocation();
+        if(!blocksPlaced.contains(location)){
+            event.setCancelled(true);
+            return;
+        }
+        blocksPlaced.remove(location);
+    }
+}
