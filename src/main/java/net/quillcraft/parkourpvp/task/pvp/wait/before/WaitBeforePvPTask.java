a609e15e646cb4a90fa98d446ba527f696cb4488
LyFlow <off.lyflow@gmail.com>
2022-04-25 23:40:27 +0200

    fix -> change delay
    add -> start pvp task
    add -> add sound
    add -> remove shop item

--- src/main/java/net/quillcraft/parkourpvp/task/pvp/wait/before/WaitBeforePvPTask.java
+++ src/main/java/net/quillcraft/parkourpvp/task/pvp/wait/before/WaitBeforePvPTask.java
@@ -1,11 +1,14 @@
 package net.quillcraft.parkourpvp.task.pvp.wait.before;
 
+import net.quillcraft.core.exception.TaskOverflowException;
 import net.quillcraft.core.task.CustomTask;
 import net.quillcraft.core.task.CustomTaskManager;
 import net.quillcraft.core.utils.Title;
 import net.quillcraft.parkourpvp.ParkourPvP;
+import net.quillcraft.parkourpvp.manager.TaskManager;
 import net.quillcraft.parkourpvp.status.InGameStatus;
-
+import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
 import org.bukkit.entity.Player;
 
 public class WaitBeforePvPTask extends CustomTask{
@@ -16,7 +19,7 @@ public class WaitBeforePvPTask extends CustomTask{
     public WaitBeforePvPTask(CustomTaskManager customTaskManager){
         super(customTaskManager);
         this.parkourPvP = ((WaitBeforePvPTaskManager)customTaskManager).getJavaPlugin();
-        this.time = 180;
+        this.time = 90;
     }
 
     @Override
@@ -24,9 +27,10 @@ public class WaitBeforePvPTask extends CustomTask{
 
         // Minute by minute
         if(time % 60 == 0 && time >= 60){
+            final int timeToDraw = time/60;
             parkourPvP.getParkourPvPGame().getPlayerUUIDList().forEach(uuid -> {
                 final Player player = parkourPvP.getServer().getPlayer(uuid);
-                final int timeToDraw = time/60;
+                player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, SoundCategory.AMBIENT, 1.0f, 0.1f);
                 new Title(player).sendFullTitle(1, 1, 1, "Le PvP commence", "dans "+timeToDraw+" minute"+((timeToDraw > 1) ? "s" : ""));
             });
         }
@@ -34,6 +38,7 @@ public class WaitBeforePvPTask extends CustomTask{
         if((time % 5 == 0 && time <= 15) || time < 5 && time > 0){
             parkourPvP.getParkourPvPGame().getPlayerUUIDList().forEach(uuid -> {
                 final Player player = parkourPvP.getServer().getPlayer(uuid);
+                player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, SoundCategory.AMBIENT, 0.5f, 2.0f);
                 new Title(player).sendActionBar("Le PvP commence dans "+time+"'s");
             });
         }
@@ -41,17 +46,19 @@ public class WaitBeforePvPTask extends CustomTask{
         if(time == 0){
             parkourPvP.getGameManager().setInGameStatus(InGameStatus.PVP);
 
-            /*try{
-                //TODO: Start PvP Timer
-                //TaskManager.JUMP_TASK_MANAGER.getCustomTaskManager().runTaskTimer(0L, 20L);
-            }catch(TaskOverflowException e){
-                e.printStackTrace();
-            }*/
-
             parkourPvP.getParkourPvPGame().getPlayerUUIDList().forEach(uuid -> {
                 final Player player = parkourPvP.getServer().getPlayer(uuid);
+                player.getInventory().clear(17);
+                player.playSound(player.getLocation(), Sound.ENTITY_LIGHTNING_BOLT_THUNDER, SoundCategory.AMBIENT, 1.0f, 1.0f);
                 new Title(player).sendActionBar("Que le meilleur gagne");
             });
+
+            try{
+                // Start PvP Timer
+                TaskManager.PVP_TASK_MANAGER.getCustomTaskManager().runTaskTimer(0L, 20L);
+            }catch(TaskOverflowException e){
+                e.printStackTrace();
+            }
 
             cancel();
             return;
