c8f127a997e84f1951d6826248bd215d417e5096
LyFlow <off.lyflow@gmail.com>
2022-04-25 23:41:20 +0200

    fix -> check if player are solo

--- src/main/java/net/quillcraft/parkourpvp/task/jump/wait/after/WaitAfterJumpTask.java
+++ src/main/java/net/quillcraft/parkourpvp/task/jump/wait/after/WaitAfterJumpTask.java
@@ -1,14 +1,24 @@
 package net.quillcraft.parkourpvp.task.jump.wait.after;
 
+import net.quillcraft.commons.game.GeneralGameStatus;
+import net.quillcraft.commons.game.ParkourPvPGame;
 import net.quillcraft.core.exception.TaskOverflowException;
 import net.quillcraft.core.task.CustomTask;
 import net.quillcraft.core.task.CustomTaskManager;
 import net.quillcraft.core.utils.MessageUtils;
+import net.quillcraft.core.utils.builders.ItemBuilder;
 import net.quillcraft.parkourpvp.ParkourPvP;
+import net.quillcraft.parkourpvp.inventory.shop.ShopCategoriesInventory;
+import net.quillcraft.parkourpvp.manager.GameManager;
 import net.quillcraft.parkourpvp.manager.TaskManager;
+import net.quillcraft.parkourpvp.scoreboard.PvPScoreboard;
 import net.quillcraft.parkourpvp.status.InGameStatus;
-import org.bukkit.Server;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class WaitAfterJumpTask extends CustomTask{
 
@@ -47,7 +57,46 @@ public class WaitAfterJumpTask extends CustomTask{
         }
 
         if(time == 0){
-            parkourPvP.getGameManager().setInGameStatus(InGameStatus.WAITING_BEFORE_PVP);
+            final Server server = parkourPvP.getServer();
+            final GameManager gameManager = parkourPvP.getGameManager();
+            final ParkourPvPGame parkourPvPGame = parkourPvP.getParkourPvPGame();
+
+            if(parkourPvPGame.getPlayerUUIDList().size() <= 1){
+                // un joueur ou personne a gagné
+                gameManager.setInGameStatus(InGameStatus.END);
+                parkourPvPGame.setGameStatus(GeneralGameStatus.END);
+
+                parkourPvPGame.updateRedis();
+
+                Bukkit.broadcastMessage("§cIl ne reste plus personne, vous avez gagné par forfait !");
+
+                try{
+                    TaskManager.END_TASK_MANAGER.getCustomTaskManager().runTaskTimer(0L, 20L);
+                }catch(TaskOverflowException e){
+                    e.printStackTrace();
+                }
+
+                cancel();
+                return;
+            }
+
+            final ArrayList<Location> spawnPvP = gameManager.getSpawnPvP();
+            final ItemStack itemStack = new ItemBuilder(Material.NETHER_STAR).setName("§b§eSHOP").toItemStack();
+            final AtomicInteger index = new AtomicInteger();
+
+            parkourPvP.getParkourPvPGame().getPlayerUUIDList().forEach(playerUUID -> {
+                final Player player = server.getPlayer(playerUUID);
+
+                new PvPScoreboard(parkourPvP).setScoreboard(player);
+
+                player.setGameMode(GameMode.SURVIVAL);
+                player.getInventory().setItem(17, itemStack);
+                player.teleport(spawnPvP.get(index.getAndIncrement()));
+                player.openInventory(new ShopCategoriesInventory().getInventory(player));
+                if(index.get() > spawnPvP.size()) index.set(0);
+            });
+
+            gameManager.setInGameStatus(InGameStatus.WAITING_BEFORE_PVP);
 
             try{
                 //Start Jump timer
