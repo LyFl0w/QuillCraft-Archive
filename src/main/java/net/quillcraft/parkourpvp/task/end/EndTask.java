4f0db90845f45de732a021cbb1949c19913c0b24
LyFlow <off.lyflow@gmail.com>
2022-04-25 23:49:31 +0200

    add -> EndTask data

--- src/main/java/net/quillcraft/parkourpvp/task/end/EndTask.java
+++ src/main/java/net/quillcraft/parkourpvp/task/end/EndTask.java
@@ -1,5 +1,7 @@
 package net.quillcraft.parkourpvp.task.end;
 
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
 import net.quillcraft.core.task.CustomTask;
 import net.quillcraft.core.task.CustomTaskManager;
 import net.quillcraft.parkourpvp.ParkourPvP;
@@ -12,13 +14,23 @@ public class EndTask extends CustomTask{
     public EndTask(CustomTaskManager customTaskManager){
         super(customTaskManager);
         this.parkourPvP = ((EndTaskManager)customTaskManager).getJavaPlugin();
-        this.time = 0;
+        this.time = 15;
     }
 
     @Override
     public void run(){
 
+        if(time == 10){
+            // TODO : dire les stats
+
+        }
+
         if(time == 0){
+            final ByteArrayDataOutput out = ByteStreams.newDataOutput();
+            out.writeUTF("Connect");
+            out.writeUTF("lobby_1");
+
+            parkourPvP.getServer().getOnlinePlayers().forEach(player -> player.sendPluginMessage(parkourPvP, "Bungeecord", out.toByteArray()));
 
             cancel();
             return;
