b4a39c568b59e1eed53c8e3938e1b5159707a0d0
LyFlow <off.lyflow@gmail.com>
2022-04-25 23:52:34 +0200

    add -> WORLDBORDER REDUCE after 3 minutes

--- src/main/java/net/quillcraft/parkourpvp/task/pvp/PvPTask.java
+++ src/main/java/net/quillcraft/parkourpvp/task/pvp/PvPTask.java
@@ -3,28 +3,48 @@ package net.quillcraft.parkourpvp.task.pvp;
 import net.quillcraft.core.task.CustomTask;
 import net.quillcraft.core.task.CustomTaskManager;
 import net.quillcraft.parkourpvp.ParkourPvP;
+import net.quillcraft.parkourpvp.scoreboard.PvPScoreboard;
 
 public class PvPTask extends CustomTask{
 
     private final ParkourPvP parkourPvP;
     private int time;
+    private final int timeToReach;
+    private final int worldBorderTime;
 
     public PvPTask(CustomTaskManager customTaskManager){
         super(customTaskManager);
         this.parkourPvP = ((PvPTaskManager)customTaskManager).getJavaPlugin();
-        this.time = 180;
+        this.time = 0;
+        this.timeToReach = 300;
+        this.worldBorderTime = 180;
     }
 
     @Override
     public void run(){
 
-        if(time == 0){
+        new PvPScoreboard(parkourPvP).updateTime();
+
+        // WORLDBORDER REDUCE
+        if(time == worldBorderTime){
+            parkourPvP.getGameManager().getWorlds()[1].getWorldBorder().setSize(0.0d, timeToReach-worldBorderTime);
+            parkourPvP.getServer().broadcastMessage("La bordure se rétrécit !");
+        }
+
+        if(time == timeToReach){
 
             cancel();
             return;
         }
 
-        time--;
+        time++;
     }
 
+    public int getTime(){
+        return time;
+    }
+
+    public int getTimeToReach(){
+        return timeToReach;
+    }
 }
