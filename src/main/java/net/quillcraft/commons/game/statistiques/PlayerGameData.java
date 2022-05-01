8625c545147f1992c3b109ca91d6c8e3450147ef
LyFlow <off.lyflow@gmail.com>
2022-05-01 13:14:14 +0200

    add -> PlayerGameData.java to store game data of a player to add it after to PlayerGameStatistique.java who saves every data in databases with PlayerGameStatistiqueProvider.java

---
+++ src/main/java/net/quillcraft/commons/game/statistiques/PlayerGameData.java
@@ -0,0 +1,41 @@
+package net.quillcraft.commons.game.statistiques;
+
+import java.util.UUID;
+
+public class PlayerGameData{
+
+    protected final String mapName, playerName;
+    protected final UUID uuid;
+    protected boolean hasWin;
+
+    public PlayerGameData(UUID uuid, String playerName, String mapName, boolean hasWin){
+        this.mapName = mapName;
+        this.playerName = playerName;
+        this.uuid = uuid;
+        this.hasWin = hasWin;
+    }
+
+    public PlayerGameData(UUID uuid, String playerName, String mapName){
+        this(uuid, playerName, mapName, false);
+    }
+
+    public String getMapName(){
+        return mapName;
+    }
+
+    public String getPlayerName(){
+        return playerName;
+    }
+
+    public UUID getUuid(){
+        return uuid;
+    }
+
+    public boolean hasWin(){
+        return hasWin;
+    }
+
+    public void setWin(){
+        this.hasWin = true;
+    }
+}
