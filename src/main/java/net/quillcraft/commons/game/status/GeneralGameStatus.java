b48cb7031f7333e6fab68e7090d5495e3374929c
LyFlow <off.lyflow@gmail.com>
2022-05-01 13:11:24 +0200

    remove useless ;

--- src/main/java/net/quillcraft/commons/game/status/GeneralGameStatus.java
+++ src/main/java/net/quillcraft/commons/game/status/GeneralGameStatus.java
@@ -2,6 +2,6 @@ package net.quillcraft.commons.game.status;
 
 public enum GeneralGameStatus{
 
-    STARTING_SERVER, PLAYER_WAITING, PLAYER_WAITING_FULL, IN_GAME, END;
+    STARTING_SERVER, PLAYER_WAITING, PLAYER_WAITING_FULL, IN_GAME, END
 
 }
