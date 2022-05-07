381fbc5752a34c56923636c8bb8864926a1ef7e1
LyFlow <off.lyflow@gmail.com>
2022-05-07 17:50:15 +0200

    register commands event to manipulate command send by player

---
+++ src/main/java/net/quillcraft/core/listener/player/PlayerCommandPreprocessListener.java
@@ -0,0 +1,35 @@
+package net.quillcraft.core.listener.player;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+import java.util.List;
+
+public class PlayerCommandPreprocessListener implements Listener{
+
+    private final List<String> commands;
+    public PlayerCommandPreprocessListener(List<String> commands){
+        this.commands = commands;
+    }
+
+    @EventHandler
+    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event){
+        final Player player = event.getPlayer();
+        if(player.isOp()) return;
+
+        final String message = event.getMessage().replaceFirst("/", "").split(" ")[0];
+        if(message.equalsIgnoreCase("help")){
+            player.sendMessage("HELP COMMAND SENT");
+            event.setCancelled(true);
+            return;
+        }
+
+        if(!commands.contains(message)){
+            player.sendMessage("Unknown command. Type \"/help\" for help");
+            event.setCancelled(true);
+        }
+    }
+
+}
---
+++ src/main/java/net/quillcraft/core/listener/player/PlayerCommandSendListener.java
@@ -0,0 +1,24 @@
+package net.quillcraft.core.listener.player;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandSendEvent;
+
+import java.util.Collection;
+import java.util.List;
+
+public class PlayerCommandSendListener implements Listener{
+
+    private final List<String> commands;
+    public PlayerCommandSendListener(List<String> commands){
+        this.commands = commands;
+    }
+
+    @EventHandler
+    public void onPlayerCommandSend(PlayerCommandSendEvent event){
+        final Collection<String> requestedCommands = event.getCommands();
+        if(!event.getPlayer().isOp()) requestedCommands.clear();
+        requestedCommands.addAll(commands);
+    }
+
+}
