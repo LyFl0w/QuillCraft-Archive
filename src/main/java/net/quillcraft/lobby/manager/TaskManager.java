29cf61af9a03f49acec6117acc3bb55c67ef6811
LyFlow <off.lyflow@gmail.com>
2022-05-27 12:15:26 +0200

    remove -> task update for muguet event
    update -> pom.xml project version

--- src/main/java/net/quillcraft/lobby/manager/TaskManager.java
+++ src/main/java/net/quillcraft/lobby/manager/TaskManager.java
@@ -1,17 +1,10 @@
 package net.quillcraft.lobby.manager;
 
-import net.quillcraft.core.utils.builders.scoreboard.ScoreboardBuilder;
 import net.quillcraft.lobby.QuillCraftLobby;
-import net.quillcraft.lobby.listener.player.PlayerJoinListener;
-import net.quillcraft.lobby.provider.MuguetProvider;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.scheduler.BukkitTask;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 public class TaskManager {
 
@@ -26,14 +19,15 @@ public class TaskManager {
     private void onEnableTasks(){
         // TODO : ADD NEW SYSTEM TASK
         // taskInProcess.add(new AutoMessageTask(quillCraftLobby).runTaskTimerAsynchronously(quillCraftLobby, 120L, 20L*60));
+        /*
         taskInProcess.add(quillCraftLobby.getServer().getScheduler().runTaskTimer(quillCraftLobby, () -> {
             final FileConfiguration muguetConfiguration = ConfigurationManager.MUGUET.getConfiguration();
-            final Set<String> timers =  muguetConfiguration.getKeys(false);
+            final Set<String> timers = muguetConfiguration.getKeys(false);
             for(String timer : timers){
                 long timerconverse = Long.parseLong(timer);
                 if(timerconverse < System.currentTimeMillis()){
-                    for(String i : muguetConfiguration.getConfigurationSection(timer+ ".").getKeys(false)){
-                        Location location = muguetConfiguration.getLocation(timer + "." + i);
+                    for(String i : muguetConfiguration.getConfigurationSection(timer+".").getKeys(false)){
+                        Location location = muguetConfiguration.getLocation(timer+"."+i);
                         location.getWorld().getBlockAt(location).setType(Material.LILY_OF_THE_VALLEY);
                         quillCraftLobby.getLogger().warning("timer path : "+timer);
                         muguetConfiguration.set(timer, null);
@@ -54,7 +48,7 @@ public class TaskManager {
             scoreboardBuilder.updateScore("muguet", 14, "Total : §e"+MuguetProvider.getSum()+" §6muguets");
             scoreboardBuilder.updateScoreboard();
 
-        },0L, 1200L));
+        },0L, 1200L));*/
 
     }
 
