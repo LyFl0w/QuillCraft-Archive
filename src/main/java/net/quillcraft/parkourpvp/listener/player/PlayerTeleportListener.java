6b711ee7eeb644e78221d71ac63aeeb61a9b4572
LyFlow <off.lyflow@gmail.com>
2023-02-03 18:11:00 +0100

    -

--- src/main/java/net/quillcraft/parkourpvp/listener/player/PlayerTeleportListener.java
+++ src/main/java/net/quillcraft/parkourpvp/listener/player/PlayerTeleportListener.java
@@ -9,18 +9,16 @@ import org.bukkit.event.player.PlayerTeleportEvent;
 public class PlayerTeleportListener implements Listener {
 
     private final ParkourPvP parkourPvP;
+    private final int maxY;
     public PlayerTeleportListener(ParkourPvP parkourPvP) {
         this.parkourPvP = parkourPvP;
+        this.maxY = parkourPvP.getGameManager().getFileConfiguration().getInt("pvp.get-max-y");
     }
 
     @EventHandler
     public void onPlayerTeleportEvent(PlayerTeleportEvent event) {
-        if(parkourPvP.getGameManager().getInGameStatus() == InGameStatus.PVP) {
-            if(event.getCause() == PlayerTeleportEvent.TeleportCause.ENDER_PEARL) {
-                if(parkourPvP.getGameManager().getFileConfiguration().getInt("pvp.get-max-y") <= event.getTo().getY()) {
-                    event.setCancelled(true);
-                }
-            }
+        if(parkourPvP.getGameManager().getInGameStatus() == InGameStatus.PVP && event.getCause() == PlayerTeleportEvent.TeleportCause.ENDER_PEARL) {
+            if(maxY <= event.getTo().getY()) event.setCancelled(true);
         }
     }
 
