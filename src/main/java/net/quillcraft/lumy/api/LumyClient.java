bff552410605322aad00b46ac7727f4c589689f2
LyFl0w <off.lyflow@gmail.com>
2024-01-24 23:26:16 +0100

    removing potential bugs

--- src/main/java/net/quillcraft/lumy/api/LumyClient.java
+++ src/main/java/net/quillcraft/lumy/api/LumyClient.java
@@ -10,81 +10,84 @@ import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class LumyClient{
+public class LumyClient {
 
     private final LinkedList<String> result;
 
-    public LumyClient(String[] args, Logger logger, File dataFolder){
+    public LumyClient(String[] args, Logger logger, File dataFolder) {
         this.result = new LinkedList<>();
 
-        try{
+        try {
             final LumyConfiguration lumyConfiguration = FileUtils.getObjectFromYamlFile(
                     FileUtils.getFileFromResource(dataFolder, "config-lumy.yml"), LumyConfiguration.class);
             start(lumyConfiguration.name(), args, logger, lumyConfiguration.ip(), lumyConfiguration.port());
-        }catch(IOException e){
+        } catch (IOException e) {
             logger.log(Level.SEVERE, e.getMessage(), e);
         }
     }
 
-    private void start(String name, String[] actions, Logger logger, String ip, int port){
-        try{
+    private void start(String name, String[] actions, Logger logger, String ip, int port) {
+        try {
             logger.info("Attempting to connect to the Lumy server");
 
-            if(Arrays.stream(actions).filter(action -> action.startsWith("name:")).count() > 1) {
+            if (Arrays.stream(actions).filter(action -> action.startsWith("name:")).count() > 1) {
                 logger.severe("You can't have multiple names");
                 return;
             }
 
-            final Socket socket = new Socket(InetAddress.getByName(ip), port);
 
-            try{
-                final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                final PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+            try (final Socket socket = new Socket(InetAddress.getByName(ip), port)) {
+                try {
+                    final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    final PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
 
-                if(name != null && !name.isBlank()) {
-                    printWriter.println("name:"+name);
-                    logger.info("Successful connection to Lumy server as "+name);
-                } else {
-                    logger.info("Successful connection to Lumy server");
-                }
+                    if (name != null && !name.isBlank()) {
+                        printWriter.println("name:" + name);
+                        logger.info("Successful connection to Lumy server as " + name);
+                    } else {
+                        logger.info("Successful connection to Lumy server");
+                    }
 
-                loop: for(String action : actions) {
-                    printWriter.println(action.toLowerCase());
+                    loop:
+                    for (final String action : actions) {
+                        printWriter.println(action.toLowerCase());
 
-                    // obtaining out streams
-                    logger.info(action+" request sent to Lumy server");
+                        // obtaining out streams
+                        logger.info(action + " request sent to Lumy server");
 
-                    if(action.startsWith("name:")) continue;
+                        if (action.startsWith("name:")) continue;
 
-                    switch(action.toLowerCase()) {
-                        case "update" -> {}
-
-                        default -> {
-                            final String respond = bufferedReader.readLine();
-
-                            if(respond.startsWith("error:")) {
-                                logger.severe(respond);
-                                break loop;
+                        switch (action.toLowerCase()) {
+                            case "update" -> {
                             }
-                            if(respond.equals("closed")) break loop;
 
-                            result.addLast(respond);
+                            default -> {
+                                final String respond = bufferedReader.readLine();
+
+                                if (respond.startsWith("error:")) {
+                                    logger.severe(respond);
+                                    break loop;
+                                }
+                                if (respond.equals("closed")) break loop;
+
+                                result.addLast(respond);
+                            }
                         }
                     }
+
+                    printWriter.close();
+                    bufferedReader.close();
+
+                    socket.close();
+                } catch (IOException e) {
+                    logger.log(Level.WARNING, e.getMessage(), e);
                 }
-
-                printWriter.close();
-                bufferedReader.close();
-
-                socket.close();
-            }catch(IOException e){
-                logger.log(Level.SEVERE, e.getMessage(), e);
             }
 
-        }catch(UnknownHostException e){
-            logger.log(Level.SEVERE, "Unknown Host", e);
-        }catch(IOException e){
-            logger.log(Level.SEVERE, "Connexion Refused", e);
+        } catch (UnknownHostException e) {
+            logger.log(Level.WARNING, "Unknown Host", e);
+        } catch (IOException e) {
+            logger.log(Level.WARNING, "Connexion Refused", e);
         }
     }
 
@@ -100,6 +103,7 @@ public class LumyClient{
         return result.poll();
     }
 
-    private record LumyConfiguration(String name, String ip, int port){}
+    private record LumyConfiguration(String name, String ip, int port) {
+    }
 
 }
--- src/main/java/net/quillcraft/lumy/api/utils/FileUtils.java
+++ src/main/java/net/quillcraft/lumy/api/utils/FileUtils.java
@@ -7,43 +7,47 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class FileUtils{
+public class FileUtils {
 
-    public static File getFileFromResource(File parent, String fileName){
+    private static final Logger logger = Logger.getLogger(FileUtils.class.getName());
+
+    public static File getFileFromResource(File parent, String fileName) {
         return getFileFromResource(new File(parent, fileName));
     }
 
-    public static File getFileFromResource(String fileName){
-        try{
+    public static File getFileFromResource(String fileName) {
+        try {
             final File file = new File(fileName);
-            if(file.getParentFile() != null && !file.getParentFile().exists()) file.getParentFile().mkdirs();
-            if(!file.exists()) Files.copy(Objects.requireNonNull(FileUtils.class.getClassLoader().getResourceAsStream(fileName)), file.toPath());
+            if (file.getParentFile() != null && !file.getParentFile().exists()) file.getParentFile().mkdirs();
+            if (!file.exists())
+                Files.copy(Objects.requireNonNull(FileUtils.class.getClassLoader().getResourceAsStream(fileName)), file.toPath());
 
             return file;
-        }catch(Exception e){
-            e.printStackTrace();
+        } catch (Exception exception) {
+            logger.log(Level.SEVERE, exception.getMessage(), exception);
         }
         return null;
     }
 
-    private static File getFileFromResource(File file){
-        try{
-            if(!file.getParentFile().exists()) file.getParentFile().mkdirs();
-            if(!file.exists()) Files.copy(Objects.requireNonNull(FileUtils.class.getClassLoader().getResourceAsStream(file.getName())), file.toPath());
+    private static File getFileFromResource(File file) {
+        try {
+            if (!file.getParentFile().exists()) file.getParentFile().mkdirs();
+            if (!file.exists())
+                Files.copy(Objects.requireNonNull(FileUtils.class.getClassLoader().getResourceAsStream(file.getName())), file.toPath());
 
             return file;
-        }catch(Exception e){
-            e.printStackTrace();
+        } catch (Exception exception) {
+            logger.log(Level.SEVERE, exception.getMessage(), exception);
         }
         return null;
     }
 
 
-    public static <T> T getObjectFromYamlFile(File file, Class<T> valueType) throws IOException{
+    public static <T> T getObjectFromYamlFile(File file, Class<T> valueType) throws IOException {
         return new ObjectMapper(new YAMLFactory()).readValue(file, valueType);
     }
-
-
 
 }
